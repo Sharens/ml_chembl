fe21c84f8e3d0f490a82a13ee34eaf967632dce9
Sharens <xarens19@gmail.com>
2025-12-11 22:54:44 +0100

    update chembl_downloader

--- libs/chembl_downloader.py
+++ libs/chembl_downloader.py
@@ -1,67 +1,94 @@
 import polars as pl
-import httpx
-import sys
-import time
+import sqlite3
 
-def get_chembl_data(batch_size=1000):
-    base_url = r"https://www.ebi.ac.uk/chembl/api/data/activity.json"
-    all_activities = []
-    offset = 0
-    total_count = 0
+DB_PATH = "libs/chembl_36/chembl_36_sqlite/chembl_36.db"
 
-    with httpx.Client(timeout=120.0) as client:
-        while True:
-            query_params = {
-                "target_organism":"Homo sapiens", 
-                "limit": batch_size,
-                "offset": offset,
-                "format": "json"
-            }
 
-            try:
-                response = httpx.get(base_url, params=query_params, timeout=120.0)
-                response.raise_for_status()
-                data = response.json()
+query = """
+SELECT
+    act.activity_id,
+    act.assay_id,
+    act.doc_id,
+    act.record_id,
+    act.molregno,
+    act.standard_relation,
+    act.standard_value,
+    act.standard_units,
+    act.standard_flag,
+    act.standard_type,
+    -- act.activity_comment, -- NOT NEEDED
+    -- act.data_validity_comment, -- NOT NEEDED
+    act.potential_duplicate,
+    act.pchembl_value,
+    act.bao_endpoint,
+    act.uo_units,
+    act.qudt_units,
+    act.toid,
+    act.upper_value,
+    act.standard_upper_value,
+    act.src_id,
+    act.type,
+    act.relation,
+    act.value,
+    act.units,
+    act.text_value,
+    act.standard_text_value
+    -- CAST(act.action_type AS STRING) AS action_type -- Wywołuje błąd
+FROM activities act
+JOIN assays ass ON act.assay_id = ass.assay_id
+JOIN target_dictionary trg ON ass.tid = trg.tid
+WHERE trg.organism = 'Homo sapiens'
+    AND act.standard_value IS NOT NULL
+LIMIT 4000000
+"""
 
-                page_meta = data.get('page_meta', {})
-                if total_count == 0:
-                    total_count = page_meta.get('total_count', 0)
-                    print(f"INFO: Downloaded {total_count} records")
-                
-                activities = data.get('activities', [])
-                if not activities:
-                    break
+polars_schema = {
+    "ACTIVITY_ID": pl.Int64,
+    "ASSAY_ID": pl.Int64,
+    "DOC_ID": pl.Int64,
+    "RECORD_ID": pl.Int64,
+    "MOLREGNO": pl.Int64,
+    "SRC_ID": pl.Int64,
+    "TOID": pl.Int64,
+    "STANDARD_RELATION": pl.String,
+    "STANDARD_VALUE": pl.Float64,
+    "STANDARD_UPPER_VALUE": pl.Float64,
+    "STANDARD_UNITS": pl.String,
+    "STANDARD_FLAG": pl.Int8,
+    "STANDARD_TYPE": pl.String,
+    "PCHEMBL_VALUE": pl.Float64,
+    "RELATION": pl.String,
+    "VALUE": pl.Float64,
+    "UPPER_VALUE": pl.Float64,
+    "UNITS": pl.String,
+    "TYPE": pl.String,
+    "TEXT_VALUE": pl.String,
+    "STANDARD_TEXT_VALUE": pl.String,
+    # "ACTIVITY_COMMENT": pl.String,
+    # "DATA_VALIDITY_COMMENT": pl.String,
+    "POTENTIAL_DUPLICATE": pl.Int8,
+    # "ACTION_TYPE": pl.String,
+    "BAO_ENDPOINT": pl.String,
+    "UO_UNITS": pl.String,
+    "QUDT_UNITS": pl.String
+}
 
-                all_activities.extend(activities)
+def extract_chembl_from_sqlite():
+    print("Nawiązywanie połączenia z bazą SQLite...")
+    conn = sqlite3.connect(DB_PATH)
 
-                print(f"INFO: Downloaded batch: {offset} - {offset + len(activities)} / {total_count}")
-                offset += batch_size
+    print("Wykonywanie zapytania i ładowanie do Polars (to może chwilę potrwać)...")
 
-                if offset >= total_count:
-                    break
-                
-                time.sleep(0.5)
-    
-            except Exception as e:
-                sys.exit(f"ERROR: Failed to retrieve data -- {e}")
-                break
 
-    return all_activities
-    
+    df = pl.read_database(query, connection=conn, schema_overrides=polars_schema)
+
+    conn.close()
+    return df
+
 if __name__ == "__main__":
-    records = get_chembl_data(batch_size=500000)
-    
-    if records:
-        print(f"\n--- Sukces! Pobrano łącznie {len(records)} rekordów. ---")
-        
-        # Tworzenie DataFrame z pełnych danych
-        # Polars świetnie radzi sobie z dużymi listami słowników
-        df = pl.DataFrame(records)
-        
-        print(f"Wymiary ramki danych: {df.shape}")
-        print(df.select(['molecule_chembl_id', 'value', 'units', 'type']).head())
-        
-        # Opcjonalnie: Zapis do pliku Parquet (dużo szybszy i lżejszy niż CSV)
-        df.write_parquet("chembl_raw_data.parquet")
-    else:
-        print("Nie pobrano żadnych danych.")
+
+    df = extract_chembl_from_sqlite()
+
+    print(f"Pobrano {df.height} rekordów.")
+    print(df.head())
+    df.write_parquet("chembl_human_activities.parquet")
